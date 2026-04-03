366c63a535284ee94e96b3cf8f928590049b1c85
Darshil Bavishi <darshilbavishi2356@gmail.com>
2026-04-03 14:43:21 +0530

    change base url

--- Frontend/src/utils/axiosConfig.js
+++ Frontend/src/utils/axiosConfig.js
@@ -1,5 +1,5 @@
-export const base_url = "http://localhost:8000/api/";
-// export const base_url = "https://mern-ecommerce-backend-g29n.onrender.com/api/";
+// export const base_url = "http://localhost:8000/api/";
+export const base_url = "https://mern-ecommerce-backend-g29n.onrender.com/api/";
 
 // Function to get config with fresh token each time
 export const getConfig = () => {

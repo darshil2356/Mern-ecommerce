c08279c9e2846b9b68ce1698ba50088a34a65a1b
Darshil Bavishi <darshilbavishi2356@gmail.com>
2026-06-15 17:02:15 +0530

    chnage map

--- Frontend/src/pages/Contact.js
+++ Frontend/src/pages/Contact.js
@@ -46,7 +46,7 @@ const Contact = () => {
         <div className="row">
           <div className="col-12">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6986.771103663534!2d76.99275607711007!3d28.886888929272477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390da5e51463d4c9%3A0xe5a485e2ac7c3d4a!2sMandaura%2C%20Haryana%20131103!5e0!3m2!1sen!2sin!4v1669909087902!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.478345535799!2d72.63545297476021!3d23.0429182791595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e87b616f4ec3f%3A0x5d6885fe7868ec3f!2sYashoda%20Fashion!5e0!3m2!1sen!2sin!4v1781523063549!5m2!1sen!2sin"
               width="600"
               height="450"
               className="border-0 w-100"
